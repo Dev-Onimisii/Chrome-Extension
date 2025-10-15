9b490c1d62ccf6a593b5fc973e3831dc27b80235
Isyak Abdulrasheed <abdulrasheedisyak44@gmail.com>
2025-10-16 00:52:52 +0100

    Updated

---
+++ Chrome-Extension/function.js
@@ -0,0 +1,8 @@
+const inputBtn = document.querySelector("#input-btn");
+
+console.log(inputBtn);
+
+inputBtn.addEventListener("click", () => {
+  e.preventDefault();
+  console.log("Button Clicked");
+});
