9c4313bb0eeead4a602094c3c0e7cc32d314a524
Isyak Abdulrasheed <abdulrasheedisyak44@gmail.com>
2025-10-16 02:14:17 +0100

    Updated

--- Chrome-Extension/function.js
+++ Chrome-Extension/function.js
@@ -2,15 +2,12 @@ const inputBtn = document.querySelector("#input-btn");
 const inputEl = document.querySelector("#input-el");
 const ulElement = document.querySelector("#ul-el");
 
-let myLeads = [];
+let myLeads = ["www.google.com", "www.youtube.com", "www.whatsapp.com"];
 
 inputBtn.addEventListener("click", (e) => {
   e.preventDefault();
   const inputElement = inputEl.value;
   myLeads.push(inputElement);
-
-  myLeads.forEach((ele) => {
-    ulElement.textContent = [ele] + " ";
-  });
-  console.log(myLeads);
 });
+
+for (let i = 0; i < myLeads.length; i++) {}
