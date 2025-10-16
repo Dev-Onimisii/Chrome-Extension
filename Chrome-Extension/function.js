1d364ca7d864fc4b073830fea4926e3c66848abe
Isyak Abdulrasheed <abdulrasheedisyak44@gmail.com>
2025-10-16 01:40:27 +0100

    Updated

--- Chrome-Extension/function.js
+++ Chrome-Extension/function.js
@@ -1,8 +1,16 @@
 const inputBtn = document.querySelector("#input-btn");
 const inputEl = document.querySelector("#input-el");
+const ulElement = document.querySelector("#ul-el");
 
 let myLeads = [];
 
 inputBtn.addEventListener("click", (e) => {
   e.preventDefault();
+  const inputElement = inputEl.value;
+  myLeads.push(inputElement);
+
+  myLeads.forEach((ele) => {
+    ulElement.textContent = ele;
+  });
+  console.log(myLeads);
 });
