5336e6b2a0ac1b797c517332350a1733da2348a2
Isyak Abdulrasheed <abdulrasheedisyak44@gmail.com>
2025-10-18 00:20:59 +0100

    Continued Updating

--- Chrome-Extension/function.js
+++ Chrome-Extension/function.js
@@ -14,7 +14,12 @@ inputBtn.addEventListener("click", (e) => {
 function renderLeads() {
   let listItems = "";
   for (let i = 0; i < myLeads.length; i++) {
-    listItems += "<li><a href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>";
+    // listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>";
+    listItems += `<li>
+                    <a target='_blank' href='${myLeads[i]}'>
+                      ${myLeads[i]}
+                    <a>
+                  </li>`;
   }
   ulElement.innerHTML = listItems;
 }
