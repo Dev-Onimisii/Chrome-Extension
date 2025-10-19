412589de3517e1c98e681715cefcf4e4f2324c00
Isyak Abdulrasheed <abdulrasheedisyak44@gmail.com>
2025-10-19 02:47:24 +0100

    Updated

--- Chrome-Extension/function.js
+++ Chrome-Extension/function.js
@@ -3,6 +3,7 @@ const inputEl = document.querySelector("#input-el");
 const ulElement = document.querySelector("#ul-el");
 
 let myLeads = [];
+localStorage.getItem("myLeads");
 
 inputBtn.addEventListener("click", (e) => {
   e.preventDefault();
