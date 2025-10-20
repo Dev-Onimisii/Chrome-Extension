b01354064fb93d9fa852ad18814fcf4dbdd6c5d0
Isyak Abdulrasheed <abdulrasheedisyak44@gmail.com>
2025-10-20 04:07:35 +0100

    Updated

--- Chrome-Extension/function.js
+++ Chrome-Extension/function.js
@@ -10,6 +10,7 @@ let leadsFromLocalStorge = JSON.parse(localStorage.getItem("myLeads"));
 const tabs = [{ url: "https://www.linkedIn.com" }];
 tabBtn.addEventListener("click", (e) => {
   e.preventDefault();
+  console.log(tabs);
 });
 
 if (leadsFromLocalStorge) {
