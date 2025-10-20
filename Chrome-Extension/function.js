16461bd701d50b309ccb1e4d9292dc05e5c233b7
Isyak Abdulrasheed <abdulrasheedisyak44@gmail.com>
2025-10-20 04:44:30 +0100

    Updated

--- Chrome-Extension/function.js
+++ Chrome-Extension/function.js
@@ -14,8 +14,8 @@ tabBtn.addEventListener("click", (e) => {
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     // since only one tab should be active and in the current window at once
     // the return variable should only have one entry
-    var activeTab = tabs[0];
-    var activeTabId = activeTab.id; // or do whatever you need
+    let activeTab = tabs[0];
+    let activeTabId = activeTab.id; // or do whatever you need
   });
 
   myLeads.push(tabs[0].url);
