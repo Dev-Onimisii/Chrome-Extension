72d63167799ed8709af805b25ea77bd443e84459
Isyak Abdulrasheed <abdulrasheedisyak44@gmail.com>
2025-10-23 23:54:34 +0100

    Updated

--- Chrome-Extension/function.js
+++ Chrome-Extension/function.js
@@ -13,9 +13,9 @@ tabBtn.addEventListener("click", (e) => {
 
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     myLeads.push(tabs[0].url);
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
   });
 
-  localStorage.setItem("myLeads", JSON.stringify(myLeads));
   render(myLeads);
 });
 
