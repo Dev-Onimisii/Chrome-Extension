92ea9905ebb5f766c0db3c2b52134daa68afef13
Isyak Abdulrasheed <abdulrasheedisyak44@gmail.com>
2025-10-26 08:15:12 +0100

    Reconnected and updated working Chrome Extension

---
+++ function.js
@@ -0,0 +1,48 @@
+const inputBtn = document.querySelector("#input-btn");
+const inputEl = document.querySelector("#input-el");
+const ulElement = document.querySelector("#ul-el");
+const deleteBtn = document.querySelector("#delete-btn");
+const tabBtn = document.querySelector("#tab-btn");
+
+let myLeads = [];
+let leadsFromLocalStorge = JSON.parse(localStorage.getItem("myLeads"));
+
+if (leadsFromLocalStorge) {
+  myLeads = leadsFromLocalStorge;
+  render(myLeads);
+}
+
+tabBtn.addEventListener("click", () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    myLeads.push(tabs[0].url);
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
+  });
+});
+
+deleteBtn.addEventListener("dblclick", () => {
+  localStorage.clear();
+  myLeads = [];
+  render(myLeads);
+});
+
+inputBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  myLeads.push(inputEl.value);
+  inputEl.value = "";
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+
+  render(myLeads);
+});
+function render(leads) {
+  let listItems = "";
+  for (let i = 0; i < leads.length; i++) {
+    listItems += `
+         <li>
+           <a target='_blank' href='${leads[i]}'>
+             ${leads[i]}
+           </a>
+           </li>`;
+  }
+  ulElement.innerHTML = listItems;
+}
